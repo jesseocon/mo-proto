52c5ed5ae9e96454bad06a994592b60bc6acf452
jesseocon <jesse.ocon@oconcompany.com>
2013-04-19 19:08:34 -0700

    added the rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoProto::Application.config.secret_token = '15f6557d4399e4136b927a18d970a11ec3c8017176b271b1a383269310715987d0e94438b0ea1d19afeb494289573804cfd9d028ca2633147f82d83ba62a29e2'
