7c888cfbf310570687ffd906d2ef4f1358469771
jesseocon <jesse.ocon@oconcompany.com>
2013-04-25 22:48:00 -0700

    Added email delivery

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,15 +1,9 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-MoProto::Application.initialize!
-
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'frozen-spire-6104.herokuapp.com',
+  :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
