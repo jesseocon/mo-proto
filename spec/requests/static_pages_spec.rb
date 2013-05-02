1c4d4dbc66511e8ee80d4f3c85469e2720952c87
jesseocon <jesse.ocon@oconcompany.com>
2013-05-01 21:56:15 -0700

    refactored spec/requests and added guard

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,11 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
   describe "Home page" do
-    it "should have the content 'Sign up!'" do
-      visit root_path
-      page.should have_link('Sign up!', href: signup_path)
-    end
-    it "should have the link 'Log in'" do
-      visit root_path
-      page.should have_link('Log in', href: signup_path)
-    end
-    it "should have the title 'Home'" do
-      visit root_path
-      page.should have_selector('title', :text => "Folioshine | Home")
-    end
+    before { visit root_path }
+    it { should have_link('Log in', href: signup_path) }
+    it { should have_link('Sign up!', href: signup_path) }
+    it { should have_selector('title', text: 'Folioshine | Home') }
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,14 +1,10 @@
 require 'spec_helper'
 
 describe "User pages" do
-  describe "Signup page" do 
-    it "should have an element h4 'Sign in" do
-      visit signup_path
-      page.should have_content('Sign in')
-    end
-    it "should have the title 'Sign up/Sign in'" do
-      visit signup_path
-      page.should have_selector('title', :text => 'Folioshine | Sign up')
-    end
+  subject { page }
+  describe "Signup page" do
+    before { visit signup_path }
+    it { should have_selector('h4', text: 'Sign in') }
+    it { should have_selector('title', text: 'Folioshine | Sign up') } 
   end
 end
