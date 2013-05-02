f855a0c1216dc45134927909fa2aac0afe666ae7
jesseocon <jesse.ocon@oconcompany.com>
2013-05-01 23:30:41 -0700

    added tests for password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,9 @@ describe User do
   
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   
   it { should be_valid }
   
@@ -57,6 +60,33 @@ describe User do
     end
     
     it { should_not be_valid }
+  end
+  
+  describe "when password is not present" do 
+    before { @user.password = @user.password_confirmation = "" }
+  end
+  
+  describe "when password is mismatch" do 
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  
+  describe "when password confirmation is nil" do 
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+  
+  describe "return value of authenticate method" do 
+    before { @user.save }
+    let (:found_user) { User.find_by_email(@user.email) }
+    
+    describe "with valid password" do 
+      it { should == found_user.authenticate(@user.password) }
+    end
+    
+    describe "with invalid password" do 
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    end
   end
 end
 
