f4c735b0bbce86ecf9f13c653739492e75ba5ae8
jesseocon <jesse.ocon@oconcompany.com>
2013-05-13 22:57:10 -0700

    removed typo from migration

--- db/migrate/20130505235714_create_packages.rb
+++ db/migrate/20130505235714_create_packages.rb
@@ -2,7 +2,7 @@ class CreatePackages < ActiveRecord::Migration
   def change
     create_table :packages do |t|
       t.string :name
-      t.decimal :price, :precision => 8, :scale => 2Pack
+      t.decimal :price, :precision => 8, :scale => 2
       t.integer :photo_qty
 
       t.timestamps
