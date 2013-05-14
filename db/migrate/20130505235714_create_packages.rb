494e81dd32866f7aca654d90876bf6fbde9d3ef4
jesseocon <jesse.ocon@oconcompany.com>
2013-05-13 22:52:03 -0700

    changed a :decimal in the CreatePackages migration

--- db/migrate/20130505235714_create_packages.rb
+++ db/migrate/20130505235714_create_packages.rb
@@ -2,7 +2,7 @@ class CreatePackages < ActiveRecord::Migration
   def change
     create_table :packages do |t|
       t.string :name
-      t.decimal :price, :decimal, :precision => 8, :scale => 2Pack
+      t.decimal :price, :precision => 8, :scale => 2Pack
       t.integer :photo_qty
 
       t.timestamps
