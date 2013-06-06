d6370638edd066049b6d060d2772d0406c81c89d
jesseocon <jesse.ocon@oconcompany.com>
2013-06-05 19:19:35 -0700

    switched from photo_url to photo

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -38,7 +38,7 @@ class AlbumsController < ApplicationController
   def get_pics
     @pics = @album.incoming_messages
     respond_to do |format|
-      format.json { render :json => @pics, :only => [:photo_url, :width, :height, :photo_file_name] }
+      format.json { render :json => @pics, :only => [:photo, :width, :height, :photo_file_name] }
     end
   end
   
