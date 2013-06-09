a82cda3a2c044f4bb346fe1e0add2be7dec931e7
jesseocon <jesse.ocon@oconcompany.com>
2013-06-09 16:36:44 -0700

    added json response in controller

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -2,5 +2,8 @@ class StarsController < ApplicationController
   def create
     @star = Star.create(params[:star])
     @star.save
+    repsond_to do |format|
+      format.json
+    end
   end
 end
