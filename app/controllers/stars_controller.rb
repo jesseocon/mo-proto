3110af7cd72c0816bb01331b465eb9c17b05dc78
jesseocon <jesse.ocon@oconcompany.com>
2013-06-09 16:57:54 -0700

    spelled respond correctly in the stars controller

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -2,7 +2,7 @@ class StarsController < ApplicationController
   def create
     @star = Star.create(params[:star])
     @star.save
-    repsond_to do |format|
+    respond_to do |format|
       format.json
     end
   end
